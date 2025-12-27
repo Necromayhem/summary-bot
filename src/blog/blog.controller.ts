be92decc85863f1bd67ffca1c60f57f70ac2d6a0
Alex <aleksanderkuklev1488@gmail.com>
2025-12-27 20:06:06 +0300

    add blog controller

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Body,
+  Delete,
+} from '@nestjs/common';
+import { BlogService } from './blog.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+  constructor(private readonly blogService: BlogService) {}
+
+  @Get()
+  findAll() {
+    return this.blogService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.blogService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreatePostDto) {
+    return this.blogService.create(dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.blogService.remove(id);
+  }
+}
